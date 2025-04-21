6eb3b9d9fac0e485ece6da8b33fd641be6296818
wishabc <aswq22013@gmail.com>
2025-04-21 19:05:13 +0700

    fixed to babachi format

--- bin/tags_to_babachi_format.py
+++ bin/tags_to_babachi_format.py
@@ -9,11 +9,11 @@ def main(in_file):
     name = os.path.basename(in_file).split('.')[0]
     with pysam.TabixFile(in_file) as vars_file:
         for line in vars_file.fetch():
-            chrom, start, end, ID, ref, alt, AAF, RAF, GT, n_ref, n_alt, n_original_reads, n_failed_mapping, n_failed_genotyping = line.strip('\n').split('\t')
+            chrom, start, end, ID, ref, alt, AAF, RAF, GT, n_ref, n_alt, n_original_reads, n_failed_mapping, n_failed_genotyping, n_failed_bias = line.strip('\n').split('\t')
             variant = SNV([chrom, start, end, ID, ref, alt, AAF, RAF, GT])
             if not variant.is_het:
                 continue
-            n_ref, n_alt, n_original_reads, n_failed_mapping, n_failed_genotyping, n_failed_bias = map(int, [n_ref, n_alt, n_original_reads, n_failed_mapping, n_failed_genotyping])
+            n_ref, n_alt, n_original_reads, n_failed_mapping, n_failed_genotyping, n_failed_bias = map(int, [n_ref, n_alt, n_original_reads, n_failed_mapping, n_failed_genotyping, n_failed_bias])
             assert n_original_reads == n_alt + n_ref + n_failed_bias + n_failed_genotyping + n_failed_mapping
 
             print('\t'.join(map(str, [chrom, start, end, ID, ref, alt, n_ref, n_alt, name, variant.aaf, variant.raf, n_failed_mapping / n_original_reads if n_original_reads > 0 else 0])))
